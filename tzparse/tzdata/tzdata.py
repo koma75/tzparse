16be41b10f870346214d1948e18cbaea4b7e0def
koma75 <okunoya@path-works.net>
2020-07-09 01:55:16 +0900

    implement all functions.

--- tzparse/tzdata/tzdata.py
+++ tzparse/tzdata/tzdata.py
@@ -142,52 +142,93 @@ def getZones(zoneFile, tzdir, clist, verbose):
             if len(row) > 3: # comment available
                 zoneList[row[2]] = {"Countries":cnlist, "Coord": row[1], "Comment": row[3]}
             else:
-                zoneList[row[2]] = {"Countries":cnlist, "Coord": row[1]}
+                zoneList[row[2]] = {"Countries":cnlist, "Coord": row[1], "Comment": ''}
 
     return zoneList
 
 def parseZone(lines, verbose):
-    pout("parseZone", verbose, Level.DEBUG)
-    pout(lines, verbose, Level.DEBUG)
-    zinfo = {}
+    pout("parseZone: {l}".format(l = lines), verbose, Level.DEBUG)
+    #pout(lines, verbose, Level.DEBUG)
+    firstLine = lines.pop(0)
+    firstLine = re.split(r'[\t ]', firstLine)
+    pout(firstLine, verbose, Level.DEBUG)
+    zinfo = {
+        firstLine[1] : {
+            "STDOFF" : firstLine[2],
+            "Rule" : firstLine[3]
+        }
+    }
+    if len(lines) > 0:
+        lastLine = re.split(r'[\t ]', lines[(len(lines)-1)].lstrip())
+        zinfo[firstLine[1]]["STDOFF"] = lastLine[0]
+        zinfo[firstLine[1]]["Rule"] = lastLine[1]
+    else:
+        pout("{zone} does not have extra lines".format(zone = firstLine[1]), verbose, Level.WARNING)
+    pout(zinfo, verbose, Level.DEBUG)
     return zinfo
+
+#Link LNKTGT LNKSRC
+#Link Asia/Bangkok Asia/Phnom_Penh	# Cambodia
+#Link Asia/Bangkok Asia/Vientiane	# Laos
 
 def parseLink(line, verbose):
-    pout("parseLink", verbose, Level.DEBUG)
-    pout(line, verbose, Level.DEBUG)
-    zinfo = {}
-    return zinfo
+    line = re.split(r'[\t ]', line)
+    pout("parseLink: {l}".format(l=line), verbose, Level.DEBUG)
+    zlink = {
+        line[2] : line[1]
+    }
+    return zlink
 
 def parseRule(line, verbose):
-    pout("parseRule", verbose, Level.DEBUG)
-    pout(line, verbose, Level.DEBUG)
-    rule = []
+    pout("parseRule: {l}".format(l=line), verbose, Level.DEBUG)
+    rule = line.split()
     return rule
+
+def expandLink(linkSrc, linkDst, zinfos, verbose):
+    pout("expanding: {src} -> {dst}".format(src=linkSrc, dst=linkDst), verbose, Level.DEBUG)
+    zinfo = {
+        linkSrc: {
+            "STDOFF" : zinfos[linkDst]["STDOFF"],
+            "Rule" : zinfos[linkDst]["Rule"],
+        }
+    }
+    pout(pformat(zinfo,depth=3,indent=4), verbose, Level.DEBUG)
+    return zinfo
 
 def parseTZDB(fpath, verbose):
     zinfos = {}
+    zlinks = {}
     rules = []
-    pout(fpath, verbose, Level.DEBUG)
+    pout("parseTZDB: {path}".format(path=fpath), verbose, Level.INFO)
     with click.open_file(fpath, 'r') as db:
         line = db.readline()
         while line:
+            # Strip comments and trailing spaces
+            line = re.sub(r'^(.*)#.*', '\\1', line).rstrip()
             if re.match(r'Zone.*',line):
                 lines = [line]
                 line = db.readline()
-                if re.match(r'\t',line):
-                    while re.match(r'\t',line):
+                # Zones span multiple lines denoted by leading tabs
+                while re.match(r'[\t\#]',line):
+                    if re.match(r'\t',line):
+                        # Strip comments and trailing spaces
+                        line = re.sub(r'^(.*)#.*', '\\1', line).rstrip()
                         lines.extend([line])
-                        line = db.readline()
+                    line = db.readline()
                 zinfos.update(parseZone(lines, verbose))
             elif re.match(r'Link.*',line):
-                zinfos.update(parseLink(line, verbose))
+                zlinks.update(parseLink(line, verbose))
                 line = db.readline()
             elif re.match(r'Rule.*',line):
-                rules.extend(parseRule(line, verbose))
+                rules.extend([parseRule(line, verbose)])
                 line = db.readline()
             else:
                 line = db.readline()
         pass
+    # Some Links source different database files.
+    pout("linking:\n{l}".format(l=zlinks), verbose, Level.DEBUG)
+    for lnk in zlinks:
+        zinfos.update(expandLink(lnk, zlinks[lnk], zinfos, verbose))
     return zinfos, rules
 
 def parseTZDBs(tzdbs, tzdir, verbose):
@@ -211,10 +252,11 @@ def parse(kwargs):
     pout(pformat(kwargs,depth=3,indent=4), verbose, Level.DEBUG)
     # 0. Get information from config.yml
     conf = yaml.safe_load(kwargs['config'])
-    pout(pformat(conf,depth=3,indent=4), verbose, Level.DEBUG)
+    pout(pformat(conf,depth=3,indent=4), verbose, Level.INFO)
 
     # 1. Parse iso3166.tab file
     #     * Hold the translation data in a Key-Value data.
+    pout("Processing contry list: {clist}".format(clist=conf['countrylist']), verbose, Level.INFO)
     clist = getCountry(conf['countrylist'], kwargs['tzdir'], verbose)
     #pout(pformat(clist, depth=2,indent=4), verbose, Level.DEBUG)
 
@@ -222,19 +264,67 @@ def parse(kwargs):
     #     * Hold the Zone information data with Key=Zone name and Value containing all other Zone definition.
     # 3. Parse all zone list file for all Links
     #     * Copy the Zone information of the linked zone to the defined zone.
-    #     * Links are always after the original Zone def. so can be parsed in one flow
     # 4. Parse all zone list file for all Rules
     #     * Can be parsed along with Zones and links
     zinfos, rules = parseTZDBs(conf['tzdata'], kwargs['tzdir'], verbose)
-    pout(pformat(zinfos, depth=3, indent=4), verbose, Level.DEBUG)
-    pout(pformat(rules, depth=3, indent=4), verbose, Level.DEBUG)
+    pout("-------Final Zone Info List--------", verbose, Level.DEBUG)
+    pout(pformat(zinfos, depth=4, indent=4), verbose, Level.DEBUG)
+    pout("-------Final Rule List--------", verbose, Level.DEBUG)
+    pout(pformat(rules, depth=2, indent=4,width=100), verbose, Level.DEBUG)
 
     # 5. Parse Zone.tab and construct the zones.csv file
+    pout("Processing zone list: {clist}".format(clist=conf['zones']), verbose, Level.INFO)
     zlist = getZones(conf['zones'], kwargs['tzdir'], clist, verbose)
+    for zone in zlist:
+        zlist[zone]["Rule"] = zinfos[zone]["Rule"]
+        zlist[zone]["STDOFF"] = zinfos[zone]["STDOFF"]
+    pout("-------Final Zone DB--------", verbose, Level.DEBUG)
     pout(pformat(zlist, depth=3,indent=4), verbose, Level.DEBUG)
 
-    # TODO: get information from zinfo and complete the data in zlist
-
     # 6. output result to csv files
+    if kwargs["overwrite"]:
+        fmode = 'w'
+    else:
+        fmode = 'x'
+
+    # 6.1 Output Rules
+    try:
+        with click.open_file(kwargs['rules'], mode=fmode) as f:
+            pout("writing {file}".format(file=kwargs['rules']), verbose, Level.INFO)
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerow(["NAME","FROM","TO","TYPE","IN","ON","AT","SAVE","LETTER/S"])
+            for rule in rules:
+                rule.pop(0)
+                writer.writerow(rule)
+            pass
+    except FileExistsError:
+        pout("{file} already exists. use '-o' to overwrite".format(file=kwargs['rules']), verbose, Level.ERROR)
+    except:
+        pout("Failed to write {file}".format(file=kwargs['rules']), verbose, Level.ERROR)
+
+    # 6.2 Output Time Zones
+    try:
+        with click.open_file(kwargs['zones'], mode=fmode) as f:
+            pout("writing {file}".format(file=kwargs['zones']), verbose, Level.INFO)
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerow(["Country","Zone","STDOFF","Rule","Coordinate","Comment"])
+            for zone in zlist:
+                for country in zlist[zone]["Countries"]:
+                    writer.writerow([
+                        country,
+                        zone,
+                        zlist[zone]["STDOFF"],
+                        zlist[zone]["Rule"],
+                        zlist[zone]["Coord"],
+                        zlist[zone]["Comment"]
+                    ])
+            pass
+    except FileExistsError:
+        pout("{file} already exists. use '-o' to overwrite".format(file=kwargs['zones']), verbose, Level.ERROR)
+    #except:
+    #    pout("Faild to write: {file}".format(file=kwargs['zones']), verbose, Level.ERROR)
+
+    # Rule	NAME	FROM	TO	TYPE	IN	ON	AT	SAVE	LETTER/S
+    # Rule	Syria	1920	1923	-	Apr	Sun>=15	2:00	1:00	S
 
     pass
